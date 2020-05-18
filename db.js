ad8ece60d7cbda5a690a9c49d45b589733ab1b8f
ㄷㅌ <62054524+exte0427@users.noreply.github.com>
2020-05-18 10:49:05 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,31 +1,45 @@
-const fs=require(`fs`);
-function db(str,type,put){
-    if(type=="delet"){
-        fs.unlink(`${str}.db`, function (err) {});
+const fs = require('fs');
+String.prototype.dbs=async function(type,add){
+    if(type!=undefined){type="main."+type;}
+    !fs.existsSync("database") && fs.mkdirSync("database"); //database 폴더가 없으면 추가
+    try{fs.readFileSync(__dirname+"\\"+'database\\'+this+".db").toString();}
+    catch(err){
+        await fs.open(__dirname+"\\"+`database\\${this+".db"}`,'w',function(err,fd){
+        });
     }
-    if(type=="make"){
-        fs.open(`${str}.db`,'w',function(err,fd){});
+    strASDffdfS=__dirname.split("\\").join("\\\\")+"\\\\"+'database\\\\'+this+".db";
+    eval(`DBstr=JSON.parse(fs.readFileSync(\`${strASDffdfS}\`).toString())`);
+    if(DBstr==""){
+        await fs.writeFile(__dirname+"\\"+'database\\'+this+".db", "{\"main\":{}}", 'utf8', function(error){console.log(error)});
     }
-    if(type=="put"){
-        if(db(str,"data")!=""){
-            fs.writeFile(`${str}.db`, fs.readFileSync(`${str}.db`).toString()+"||||||||||"+`\`${put}\``, 'utf8', function(error){});
+    if(type==undefined){
+        return fs.readFileSync(__dirname+"\\"+'database\\'+this+".db").toString();
+    }
+    else if(type.indexOf("add")!=-1){
+        let file=type.replace(".add","");
+        let y="";
+        for(let i=0;i<file.split(".").length-1;i++){
+            y=y+file.split(".")[i];
         }
-        else{
-            fs.writeFile(`${str}.db`, `\`${put}\``, 'utf8', function(error){});
-        }
+        await eval(`DBstr.${y}={}`);
+        await eval(`DBstr.${file} = '${add}'`);
+        console.log(y);
     }
-    if(type=="data"){
-        eval(`jsonDATA=[${fs.readFileSync(`${str}.db`).toString().split("||||||||||")}]`);
-        return jsonDATA;
+    else if(type.indexOf("set")!=-1){
+        let file=type.replace(".set","");
+        await eval(`DBstr.${file}='${add}'`);
+    }
+    else if(type.indexOf("del")!=-1){
+        let file=type.replace(".del","");
+        await eval(`delete DBstr.${file}`);
     }
     else{
-        type=type*1;
-        let v=db(str,"data");
-        for(let i=0;i<v.length;i++){
-            if(i==type){
-                v[i]=put;
-            }
-        }
-        fs.writeFile(`${str}.db`, "`"+v.join("||||||||||")+"`", 'utf8', function(error){});
+        return eval(`DBstr.${type}`);
     }
+    fs.writeFile(__dirname+"\\"+`database\\${this+".db"}`, JSON.stringify(DBstr), 'utf8', function(error){console.log(error)});
 }
+String.prototype.db=function(type,add){
+    let a=this.dbs(type,add);
+    return a;
+}
+console.log("ex".db("aa"));
